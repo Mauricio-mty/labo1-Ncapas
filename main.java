9dcb37e795cd9dbf4731492d8222e77a09819925
Mauricio-mty <00000422@uca.edu.sv>
2025-04-05 10:50:37 -0600

    menu

--- main.java
+++ main.java
@@ -1,9 +1,34 @@
-public class main {
+import Services.ventaService;
+import java.util.Scanner;
+
+
+public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-              //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-       System.out.println("/n Hello World");
+        boolean O=true;
+      Scanner s = new Scanner(System.in);
+      ventaService venta = new ventaService();
+
+       do {
+           System.out.println("""
+                              Gestion liberia
+                              Tepresento las siguientes opciones
+                              1-Nueva Venta
+                              2-Listar ventas
+                              3-Detalle de venta
+                              4-Salir""");
+           int entero = s.nextInt();
+            switch (entero){
+                case 1:  venta.newVenta(s);
+                    break;
+                case 2:
+                case 3:
+                case 4: O=false; break;
+                default:System.out.println("Opcion no valida");
+            }
+       }while (O);
+
+           s.close();
+        //int entero = s.nextInt();
+
     }
 }
